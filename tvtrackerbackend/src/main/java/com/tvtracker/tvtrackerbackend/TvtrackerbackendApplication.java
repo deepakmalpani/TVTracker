2e0292261b9a73b140258f783aacd933c54c63ee
Deepak Malpani <deepakmalpani0@gmail.com>
2023-08-22 19:44:37 +0530

    Adding spring initial setup

---
+++ tvtrackerbackend/src/main/java/com/tvtracker/tvtrackerbackend/TvtrackerbackendApplication.java
@@ -0,0 +1,13 @@
+package com.tvtracker.tvtrackerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TvtrackerbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TvtrackerbackendApplication.class, args);
+	}
+
+}
---
+++ tvtrackerbackend/src/test/java/com/tvtracker/tvtrackerbackend/TvtrackerbackendApplicationTests.java
@@ -0,0 +1,14 @@
+package com.tvtracker.tvtrackerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TvtrackerbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+		
+	}
+
+}
