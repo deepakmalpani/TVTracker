4993f6020a703e93e28b9c77ea9fac69ce89d425
Deepak Malpani <deepakmalpani0@gmail.com>
2023-08-23 12:13:32 +0530

    Init search movie controller

---
+++ tvtrackerbackend/src/main/java/com/tvtracker/tvtrackerbackend/controllers/SearchMovieController.java
@@ -0,0 +1,14 @@
+package com.tvtracker.tvtrackerbackend.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class SearchMovieController {
+    
+    @RequestMapping("/")
+    public @ResponseBody String display(){
+        return "Hello, World";
+    }
+}
