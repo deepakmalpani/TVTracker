c17bc4c6d1173f495e0369bb67b0a3dcaa3f8185
Deepak Malpani <deepakmalpani0@gmail.com>
2023-08-26 16:07:40 +0530

    Adding get request for movie

--- tvtrackerbackend/src/main/java/com/tvtracker/tvtrackerbackend/controllers/MovieDetailsController.java
+++ tvtrackerbackend/src/main/java/com/tvtracker/tvtrackerbackend/controllers/MovieDetailsController.java
@@ -1,14 +1,32 @@
 package com.tvtracker.tvtrackerbackend.controllers;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.GetMapping;
+import java.util.Optional;
 
-@Controller
+import com.tvtracker.tvtrackerbackend.repositories.MovieRepository;
+import com.tvtracker.tvtrackerbackend.models.MovieModel;
+
+@RestController
+@RequestMapping("/moviedetails")
 public class MovieDetailsController{
     
-    @RequestMapping("/")
-    public @ResponseBody String display(){
-        return "Hello, World";
+    private MovieRepository movieRepository;
+
+    public MovieDetailsController(MovieRepository movieRepository){
+        this.movieRepository = movieRepository;
+    }
+
+    @GetMapping("/{imdbId}")
+    public ResponseEntity<MovieModel> findByimdbId(@PathVariable String imdbId){
+        Optional<MovieModel> movieModelOptional = Optional.of(movieRepository.findByimdbId(imdbId));
+        if (movieModelOptional.isPresent()){
+            return ResponseEntity.ok(movieModelOptional.get());
+        } else{
+            return ResponseEntity.notFound().build();
+        }
     }
 }
