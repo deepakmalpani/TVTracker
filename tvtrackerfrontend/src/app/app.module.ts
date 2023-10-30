6eb064e23ff205ed0009e702f4dc0e4af7ea0445
Deepak Malpani <deepak.malpani@exxonmobil.com>
2023-10-30 18:06:41 +0530

    Updating app root component
    Updating app root component

--- tvtrackerfrontend/src/app/app.module.ts
+++ tvtrackerfrontend/src/app/app.module.ts
@@ -7,10 +7,18 @@ import { MovieSearchComponent } from './movie/movie-search.component';
 import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 
+import { AuthModule } from '@auth0/auth0-angular';
+import { environment as env } from '../environments/environment';
+import { HomeComponent } from './home/home.component';
+import { SharedModule } from './shared/shared.module';
+import { ProfileComponent } from './profile/profile.component';
+
 @NgModule({
   declarations: [
     AppComponent,
-    MovieSearchComponent
+    MovieSearchComponent,
+    HomeComponent,
+    ProfileComponent,
   ],
   imports: [
     BrowserModule,
@@ -18,9 +26,17 @@ import { FormsModule } from '@angular/forms';
     HttpClientModule,
     RouterModule.forRoot([
       { path: 'movie-search', component: MovieSearchComponent },
+      { path: 'profile', component: ProfileComponent},
       { path: '', redirectTo: 'movie-search', pathMatch: 'full' },
       { path: '**', redirectTo: 'movie-search', pathMatch: 'full' }
-    ])  
+    ]),
+    AuthModule.forRoot({
+      ...env.auth,
+      authorizationParams: {
+        redirect_uri : env.auth.redirectUri
+      }
+    }),
+    SharedModule  
   ],
   providers: [],
   bootstrap: [AppComponent]
